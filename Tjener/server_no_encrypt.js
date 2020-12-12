6fad007fd24c46e259365490fc27cb0d74968706
SimenDS <smndagersneve@gmail.com>
2020-12-12 15:06:26 +0100

    Updated cors implementation

--- Tjener/server_no_encrypt.js
+++ Tjener/server_no_encrypt.js
@@ -10,7 +10,7 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
  
-app.listen(80, function () {
+app.listen(2520, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
 
