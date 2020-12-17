d0e44ed5f5a86fd66027d49267eae227436e1e8c
Simends <72865099+Simends@users.noreply.github.com>
2020-12-17 13:38:26 +0100

    Delete script.js

--- Nettside/script/script.js
+++
@@ -1,3 +0,0 @@
-//Start with defining all the key elements on the webpage.
-//What we are doing here is putting the HTML element/tag into an actual variable/object in the "functions-language" JavaScript
-console.log("Starter script");
